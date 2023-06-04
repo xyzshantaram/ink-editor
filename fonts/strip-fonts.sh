14a11063c297d7e1ec8d772ba9db41215de7a77d
Siddharth Singh <me@shantaram.xyz>
2023-06-05 00:59:12 +0530

    switch nerd font build source

--- fonts/strip-fonts.sh
+++ fonts/strip-fonts.sh
@@ -4,7 +4,7 @@ tmpfile="$(mktemp)"
 echo $tmpfile
 
 uni2ascii -a P icons.txt > $tmpfile
-pyftsubset HurmitNerdFont-Regular.otf --output-file=NerdFont-stripped.ttf \
+pyftsubset GoMonoNerdFont-Regular.ttf --output-file=NerdFont-stripped.ttf \
     --layout-features='*' --glyph-names --symbol-cmap --legacy-cmap \
     --notdef-glyph --notdef-outline \
     --name-IDs='*' --name-legacy --name-languages='*' \
