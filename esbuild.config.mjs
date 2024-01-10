ab0c9c053d3dcc53953f58a92a97d9a5767cc4e3
Siddharth Singh <me@shantaram.xyz>
2024-01-10 14:06:20 +0530

    build with typescript

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,7 +1,7 @@
 import esbuild from "esbuild"
 
 await esbuild.build({
-    entryPoints: ['src/mod.ts'],
+    entryPoints: ['ts-output/index.js'],
     bundle: true,
     outfile: './dist/ink-editor.js',
     platform: 'browser',
